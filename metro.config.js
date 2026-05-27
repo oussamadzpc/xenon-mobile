819515c772eae3bdd13b80480381e29fd612ad79
oussamadzpc <oussamadzpc@gmail.com>
2026-05-27 13:56:42 +0100

    Fix Hermes dynamic import error

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Fix Hermes dynamic import issues
+config.transformer.minifierConfig = {
+  keep_fnames: true,
+  mangle: false,
+  compress: false,
+};
+
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
+
 module.exports = config;
